7b03cac09980dedaf95b93cf72ab542d03ca5a0e
StevenDrHR <0906986@hr.nl>
2016-01-25 12:26:12 +0100

    Test2
    testing testing

---
+++ Survivor v0.2/Assets.py
@@ -0,0 +1,34 @@
+﻿import pygame
+import sys
+from win32api import GetSystemMetrics
+
+
+##assets
+#if size == (1600, 900):
+#    Wallpaper = pygame.image.load("MenuWallpaper900p.png")
+#if size == (1920, 1080):
+#    Wallpaper = pygame.image.load("MenWallpaper1080p.png")
+#QuitButton = pygame.image.load("QuitButton.png")
+#StartButton = pygame.image.load("StartButton.png")
+
+
+##wallpaper logic
+#def wall(xw,yw):
+#    screen.blit(Wallpaper,(xw,yw))
+
+#xw = (width * 0)
+#yw= (height * 0)
+
+##QuitButton logic
+#def quit(xq,yq):
+#    screen.blit(QuitButton,(xq,yq))
+
+#xq = (width * 0.6)
+#yq = (height * 0.6)
+
+##StartButton logic
+#def start(xs,ys):
+#    screen.blit(StartButton,(xs,ys))
+
+#xs = (width * 0.6)
+#ys = (height * 0.4)
