958c80348072ac59745f9bc80407693fe78dd638
WillemvdPijl <willemvanderpijl@hotmail.com>
2016-01-27 13:00:37 +0100

    Windowed Version

--- Survivor v0.2/Survivor v0.1/Survivor_v0.2.py
+++ Survivor v0.2/Survivor v0.1/Survivor_v0.2.py
@@ -1,47 +1,49 @@
+﻿
 print("Welcome sir")
 import pygame
 import sys
 import os
-from time import*
+from time import *
 from Assets import *
 from pygame.locals import *
-from win32api import GetSystemMetrics
-
-print ("Width =", GetSystemMetrics(0))
-print ("Height =", GetSystemMetrics(1))
 
 #window size
 pygame.init()
-size = width, height = GetSystemMetrics(0),GetSystemMetrics(1)
+width = 1100
+height = 600
+size = ((width,height))
 screen = pygame.display.set_mode(size)
+white = (255,255,255)
+black = (0,0,0)
+red = (255,0,0)
+blue = (20,66,122)
 offset = 50
 board_size = 10
 pygame.display.set_caption('Survivor')
-
-
-
+boardgame = pygame.image.load ("TheBoard.png")
+gameDisplay = pygame.display.set_mode(size)
+Player1 = pygame.image.load("BoksHandschoen3.png")
+Player2 = pygame.image.load("BoksHandschoen.png")
+Player3 = pygame.image.load("BoksHandschoen4.png")
+Player4 = pygame.image.load("BoksHandschoen2.png")
 
 #assets
-if size == (1600, 900):
-    Wallpaper = pygame.image.load("MenuWallpaper900p.png")
-if size == (1920, 1080):
-    Wallpaper = pygame.image.load("MenuWallpaper1080p.png")
-if size == (1366,768):
-    Wallpaper = pygame.image.load("MenuWallpaper796p.png")
-if size == (1600, 900):
-    Board = pygame.image.load("BoardGame900p.png")
-if size == (1920, 1080):
-    Board = pygame.image.load("BoardGame1080p.png")
+Wallpaper = pygame.image.load("WallpaperW.png")
 QuitButton = pygame.image.load("QuitButton.png")
 QuitB = pygame.image.load("QuitButton.png")
+QuitButton2 = pygame.image.load("QuitButton2.png")
 StartButton = pygame.image.load("StartButton.png")
 StartB = pygame.image.load("StartButton.png")
+StartButton2 = pygame.image.load("StartButton2.png")
 ManualButton = pygame.image.load("ManualButton.png")
 ManualB = pygame.image.load("ManualButton.png")
-LogoCorner = pygame.image.load("LogoSmall.png")
+ManualButton2 = pygame.image.load("ManualButton2.png")
+
+crashed = False
+gameExit = False
+
 
 #Music
-#music
 pygame.mixer.init()
 pygame.mixer.music.load("TitleMusic.mp3",)
 pygame.mixer.music.play()
@@ -54,11 +56,11 @@ def wall(xw,yw):
     screen.blit(Wallpaper,(xw,yw))
 
 #QuitButton logic
-def Quit(xq,yq):
+def Quit(xq,yq,QuitButton):
     screen.blit(QuitButton,(xq,yq))
 
 #StartButton logic
-def Start(xs,ys):
+def Start(xs,ys,StartButton):
     screen.blit(StartButton,(xs,ys))
 
 #Logo logic
@@ -66,174 +68,138 @@ def Logo(xl,yl):
     screen.blit(LogoCorner,(xl,yl))
 
 #ManualButton logic
-def Manual(xm,ym):
+def Manual(xm,ym,ManualButton):
     screen.blit(ManualButton,(xm,ym))
 
-#hierdoor blijft t beeld staan for some reason + Fullscreen
+#Game assets logic
 def CornerTile(x,y):
     x = (0)
     y = (0)
     gameDisplay.blit(boardgame,(x,y))
 
-def Player1Glove(List):
-    x = PlayerList1.Position_X
-    y = PlayerList1.Position_Y
+def Player1Glove(l1):
+    x = l1.Position_X
+    y = l1.Position_Y
     gameDisplay.blit(Player1,(x,y))
 
-def Player2Glove(List):
-    x = PlayerList2.Position_X
-    y = PlayerList2.Position_Y
+def Player2Glove(l2):
+    x = l2.Position_X
+    y = l2.Position_Y
     gameDisplay.blit(Player2,(x,y))
 
-def Player3Glove(List):
-    x = PlayerList3.Position_X
-    y = PlayerList3.Position_Y
+def Player3Glove(l3):
+    x = l3.Position_X
+    y = l3.Position_Y
     gameDisplay.blit(Player3,(x,y))
-def Player4Glove(List):
-    x = PlayerList4.Position_X
-    y = PlayerList4.Position_Y
+def Player4Glove(l4):
+    x = l4.Position_X
+    y = l4.Position_Y
     gameDisplay.blit(Player4,(x,y))
-Willem is raar
-DISPLAYSURF = pygame.display.set_mode((0,0),FULLSCREEN)
-gameExit = True
-crashed = False
-while not crashed:
-    xs = (width * 0.6)
-    ys = (height * 0.2)
-    xm = (width * 0.6)
+
+#MAINMENU
+def MainMenu(crash,Exit,ManualButton, QuitButton,StartButton):
+    crash = False
+    xs = (width * 0.5)
+    ys = (height * 0.10)
+    xm = (width * 0.5)
     ym = (height * 0.4)
-    xq = (width * 0.6)
-    yq = (height * 0.6)
+    xq = (width * 0.5)
+    yq = (height * 0.7)
     xw = (width * 0)
     yw = (height * 0)
     xl = (width * 0.8)
     yl = (height * 0.8)
-   
-    for event in pygame.event.get():
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                gameExit = False
-                crashed = True
-        if event.type == pygame.QUIT:
-            crashed = True
+    while not crash:
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    crash = True
+            if event.type == pygame.QUIT:
+                crash = True
+            elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
+                crash = True
+        else:
+            if crash == False:
+                wall(xw,yw)
+                Quit(xq,yq,QuitButton)
+                Start(xs,ys,StartButton)
+                Manual(xm,ym,ManualButton)
+                for event in pygame.event.get():
+                    if event.type == MOUSEMOTION:
+                        mousex, mousey = pygame.mouse.get_pos()
 
-        elif event.type == pygame.KEYDOWN and event.key == pygame.K_ESCAPE:
-            crashed = True
-
-    else:
-        if crashed == False:
-            wall(xw,yw)
-            Quit(xq,yq)
-            Start(xs,ys)
-            #Logo(xl,yl)
-            Manual(xm,ym)
-
-
-            for event in pygame.event.get():
-                if event.type == MOUSEMOTION:
-                    mousex, mousey = pygame.mouse.get_pos()
-
-                    ####Lijst van buttons met class maken dat je makkelijk een button kan toewijzen.
-
-                    #Mouse hoover
-            if size == (1600, 900):
-                if event.type == MOUSEMOTION and mousex > 960 and mousex < 1360  and mousey > 540 and mousey < 690:
-                    QuitButton = pygame.image.load("QuitButton2.png")
+                #MOUSE HOOVER
+                if event.type == MOUSEMOTION and mousex > 550 and mousex < 950  and mousey > 60 and mousey < 210:
+                        StartButton = StartButton2
                 else:
-                    QuitButton = QuitB
-                if event.type == MOUSEMOTION and mousex > 960 and mousex < 1360  and mousey > 180 and mousey < 330:
-                    StartButton = pygame.image.load("StartButton2.png")
+                        StartButton = StartB
+                if event.type == MOUSEMOTION and mousex > 550 and mousex < 950  and mousey > 240 and mousey < 390:
+                        ManualButton = ManualButton2
                 else:
-                    StartButton = StartB
-                if event.type == MOUSEMOTION and mousex > 960 and mousex < 1360  and mousey > 360 and mousey < 510:
-                    ManualButton = pygame.image.load("ManualButton2.png")
+                        ManualButton = ManualB
+                if event.type == MOUSEMOTION and mousex > 550 and mousex < 950  and mousey > 420 and mousey < 570:
+                        QuitButton = QuitButton2
                 else:
-                    ManualButton = ManualB
+                        QuitButton = QuitB
 
-
-                #Scaling 
-####################################################################################################################################################################################
-            if size == (1920,1080):             #1080p
-                if event.type == MOUSEBUTTONUP and mousex > 1152 and mousex < 1552  and mousey > 216 and mousey < 366:
-                    crashed = True      #Fight
-                    gameExit = False
-                if event.type == MOUSEBUTTONUP and mousex > 1152 and mousex < 1552  and mousey > 648 and mousey < 798:
-                    crashed = True      #Quit
-                if event.type == MOUSEBUTTONUP and mousex > 1152 and mousex < 1552  and mousey > 432 and mousey < 582:
+                #KNOP INTERACTIE 
+                if event.type == MOUSEBUTTONUP and mousex > 550 and mousex < 950  and mousey > 60 and mousey < 210:
+                    Exit = False #fight
+                    BoardGame(PlayerList1,PlayerList2,PlayerList3,PlayerList4,Exit,crash)
+                if event.type == MOUSEBUTTONUP and mousex > 550 and mousex < 950  and mousey > 240 and mousey < 390:
                     print("Manual is still missing")      #Manual
-
-            if size == (1600, 900):             #900p
-                if event.type == MOUSEBUTTONUP and mousex > 960 and mousex < 1360  and mousey > 180 and mousey < 330:
-                    crashed = True      #Fight
-                    gameExit = False
-                if event.type == MOUSEBUTTONUP and mousex > 960 and mousex < 1360  and mousey > 540 and mousey < 690:
-                    crashed = True      #Quit
-                if event.type == MOUSEBUTTONUP and mousex > 960 and mousex < 1360  and mousey > 360 and mousey < 510:
-                    print("Manual is still missing")    #Manual
-
-            if size == (1366, 768):             #768
-                if event.type == MOUSEBUTTONUP and mousex > 820 and mousex < 1360  and mousey > 154 and mousey < 304:
-                    crashed = True
-                    gameExit = False  #Fight
-                if event.type == MOUSEBUTTONUP and mousex > 820 and mousex < 1220  and mousey > 461 and mousey < 611:
-                    crashed = True     #quit
-                if event.type == MOUSEBUTTONUP and mousex > 820 and mousex < 1360  and mousey > 307 and mousey < 457:
-                    print("Manual is still missing")  #Manual
-
-            pygame.display.update()
-
-            
+                    
+                if event.type == MOUSEBUTTONUP and mousex > 550 and mousex < 950  and mousey > 420 and mousey < 570:
+                    pygame.quit()
+                    quit()
+                        
+        pygame.display.update()
     clock.tick(60)
 
 
-display_width = 600
-display_height = 800
-white = (255,255,255)
-black = (0,0,0)
-red = (255,0,0)
-boardgame = pygame.image.load ("Board.png")
-gameDisplay = pygame.display.set_mode((display_height,display_width))
-pygame.display.set_caption('Survivor')
-Player1 = pygame.image.load("BoksHandschoen3.png")
-Player2 = pygame.image.load("BoksHandschoen.png")
-Player3 = pygame.image.load("BoksHandschoen4.png")
-Player4 = pygame.image.load("BoksHandschoen2.png")
-DISPLAYSURF = pygame.display.set_mode((0,0),FULLSCREEN)
 
-while not gameExit:
-    
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            gameExit = True
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_ESCAPE:
-                gameExit = True
-            if event.key == pygame.K_1:
-                PlayerList1 = PlayerList1.tail
-            if event.key == pygame.K_2:
-                PlayerList2 = PlayerList2.tail
-            if event.key == pygame.K_3:
-                PlayerList3 = PlayerList3.tail
-            if event.key == pygame.K_4:
-                PlayerList4 = PlayerList4.tail
-    if PlayerList1.Name == "Pos41":
-        PlayerList1 = PlayerPosList1
-    if PlayerList2.Name == "Pos11":
-        PlayerList2 = PlayerPosList2
-    if PlayerList3.Name == "Pos21":
-        PlayerList3 = PlayerPosList3
-    if PlayerList4.Name == "Pos31":
-        PlayerList4 = PlayerPosList4
-    gameDisplay.fill(white)
-    CornerTile(0,0)
-    Player1Glove(PlayerList1)
-    Player2Glove(PlayerList2)
-    Player3Glove(PlayerList3)
-    Player4Glove(PlayerList4)
-    pygame.display.flip()
-    print("Hello")
+def BoardGame(l1,l2,l3,l4,Exit,crash):
+    pygame.display.set_mode(size)
+    Exit = False
+    pygame.display.set_caption('Survivor')
+    while not Exit:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                Exit = True
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_ESCAPE:
+                    MainMenu(crash,Exit,ManualButton, QuitButton,StartButton)
+                    
+                if event.key == pygame.K_1:
+                    print (event)
+                    l1 = l1.tail
+                if event.key == pygame.K_2:
+                    print (event)
+                    l2 = l2.tail
+                if event.key == pygame.K_3:
+                    print (event)
+                    l3 = l3.tail
+                if event.key == pygame.K_4:
+                    print (event)
+                    l4 = l4.tail
+        if l1.Name == "Pos41":
+            l1 = PlayerPosList1
+        if l2.Name == "Pos11":
+            l2 = PlayerPosList2
+        if l3.Name == "Pos21":
+            l3 = PlayerPosList3
+        if l4.Name == "Pos31":
+            l4 = PlayerPosList4
+        gameDisplay.fill(blue)
+        CornerTile(0,0)
+        Player1Glove(l1)
+        Player2Glove(l2)
+        Player3Glove(l3)
+        Player4Glove(l4)
+        pygame.display.update()
 
+# Aanroepencode
+MainMenu(crashed,gameExit,ManualButton, QuitButton,StartButton)
 pygame.quit()
 quit()
-
 
